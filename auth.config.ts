7bdbdc8c9787b2215bc964a8d913daa8163ca050
Julius Lave Konge <jukon23@student.sdu.dk>
2024-11-21 00:39:28 +0100

    Fix again

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,7 @@
-import Google from "next-auth/providers/Google"
+import GoogleProvider from "next-auth/providers/Google"
 import type { NextAuthConfig } from "next-auth"
  
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [Google],
+  providers: [GoogleProvider],
 } satisfies NextAuthConfig
