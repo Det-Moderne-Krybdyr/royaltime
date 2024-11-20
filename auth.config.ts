958059c776980d494e942351c1825d569607af79
Julius Lave Konge <jukon23@student.sdu.dk>
2024-11-21 00:42:28 +0100

    Again

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,12 @@
-import GoogleProvider from "next-auth/providers/Google"
-import type { NextAuthConfig } from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+import type { NextAuthConfig } from "next-auth";
  
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GoogleProvider],
-} satisfies NextAuthConfig
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+} satisfies NextAuthConfig;
